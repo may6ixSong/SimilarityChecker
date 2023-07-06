049ea2f57844900d5acec7c628d911ece73e0a2b
may6ix.song <smh92996275@gmail.com>
2023-07-06 11:38:23 +0900

    Length 구현

--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -1,5 +1,28 @@
+#include <string>
+using namespace std;
+
 class SimilarityChecker
 {
 public:
+	float getLengthPoint(string input1, string input2)
+	{
+		int input1_length = input1.size();
+		int input2_length = input2.size();
 
+		if (input1_length == input2_length) return 60;
+		else if (input1_length >= input2_length * 2
+			|| input1_length * 2 <= input2_length) return 0;
+
+		// 부분 점수
+		if (input1_length > input2_length)
+			return getPartPoint(input1_length, input2_length);
+		else
+			return getPartPoint(input2_length, input1_length);
+	}
+
+	float getPartPoint(int long_length, int short_length)
+	{
+		int gap = long_length - short_length;
+		return 60 - (gap * 60 / short_length);
+	}
 };
--- SimilarityChecker_TEST/SimilarityChecker_test.cpp
+++ SimilarityChecker_TEST/SimilarityChecker_test.cpp
@@ -1,7 +1,39 @@
 #include "pch.h"
+#include "string.h"
 #include "../SimilarityChecker/SimilarityChecker.cpp"
 
-TEST(TestCaseName, TestName) {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+TEST(SimilarityCheckerTest, ReturnMaxLengthPoint) {
+	SimilarityChecker cal;
+	std::string input1 = "ABC";
+	std::string input2 = "BBY";
+	int ret = cal.getLengthPoint(input1, input2);
+
+	EXPECT_EQ(60, ret);
+}
+
+TEST(SimilarityCheckerTest, ReturnZeroLengthPoint) {
+	SimilarityChecker cal;
+	std::string input1 = "A";
+	std::string input2 = "BB";
+	int ret = cal.getLengthPoint(input1, input2);
+
+	EXPECT_EQ(0, ret);
+}
+
+TEST(SimilarityCheckerTest, Return20LengthPoint) {
+	SimilarityChecker cal;
+	std::string input1 = "AAABB";
+	std::string input2 = "BAA";
+	int ret = cal.getLengthPoint(input1, input2);
+
+	EXPECT_EQ(20, ret);
+}
+
+TEST(SimilarityCheckerTest, Return30LengthPoint) {
+	SimilarityChecker cal;
+	std::string input1 = "AA";
+	std::string input2 = "AAE";
+	int ret = cal.getLengthPoint(input1, input2);
+
+	EXPECT_EQ(30, ret);
 }
