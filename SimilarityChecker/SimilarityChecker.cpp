3762b63ea01d746d8e78b388c0f206afb29b39ca
may6ix.song <smh92996275@gmail.com>
2023-07-06 13:27:11 +0900

    등장 Alphabet검사 구현

--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -1,5 +1,40 @@
+#include <string>
+#include <set>
+using namespace std;
+
 class SimilarityChecker
 {
 public:
+	int getAlphaPoint(string input1, string input2)
+	{
+		set<char> Alphabet;
+		for(char ch : input1)
+		{
+			Alphabet.insert(ch);
+		}
+		for (char ch : input2)
+		{
+			Alphabet.insert(ch);
+		}
 
+		int totalCnt = Alphabet.size();
+		int sameCnt = 0;
+		for(char alpha : Alphabet)
+		{
+			if (!checkCharHave(input1, alpha)) continue;
+			if (!checkCharHave(input2, alpha)) continue;
+			sameCnt++;
+		}
+
+		return (40 * sameCnt) / totalCnt;
+	}
+
+	bool checkCharHave(string input, char checkAlphabet)
+	{
+		for(char ch : input)
+		{
+			if (checkAlphabet == ch) return true;
+		}
+		return false;
+	}
 };
--- SimilarityChecker_TEST/SimilarityChecker_test.cpp
+++ SimilarityChecker_TEST/SimilarityChecker_test.cpp
@@ -1,7 +1,39 @@
 #include "pch.h"
+#include "string.h"
 #include "../SimilarityChecker/SimilarityChecker.cpp"
 
-TEST(TestCaseName, TestName) {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+TEST(SimilarityCheckerTest, ReturnMaxAlphaPoint) {
+	SimilarityChecker cal;
+	std::string input1 = "ASD";
+	std::string input2 = "DSA";
+	int ret = cal.getAlphaPoint(input1, input2);
+
+	EXPECT_EQ(40, ret);
+}
+
+TEST(SimilarityCheckerTest, ReturnZeroAlphaPoint) {
+	SimilarityChecker cal;
+	std::string input1 = "A";
+	std::string input2 = "BB";
+	int ret = cal.getAlphaPoint(input1, input2);
+
+	EXPECT_EQ(0, ret);
+}
+
+TEST(SimilarityCheckerTest, Return40AlphaPoint) {
+	SimilarityChecker cal;
+	std::string input1 = "AAAABB";
+	std::string input2 = "BA";
+	int ret = cal.getAlphaPoint(input1, input2);
+
+	EXPECT_EQ(40, ret);
+}
+
+TEST(SimilarityCheckerTest, Return20AlphaPoint) {
+	SimilarityChecker cal;
+	std::string input1 = "AA";
+	std::string input2 = "AAE";
+	int ret = cal.getAlphaPoint(input1, input2);
+
+	EXPECT_EQ(20, ret);
 }
